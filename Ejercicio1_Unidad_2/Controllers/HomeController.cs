a184b56384cef45de32b7c94f2533599f68790ae
Ronald-PN <45144094+Ronald-PN@users.noreply.github.com>
2018-12-19 02:28:52 +0100

    Initial commit

---
+++ Ejercicio1_Unidad_2/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ejercicio1_Unidad_2.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return "<html><body>" +
+                "<h1> UNIVERSIDAD AUTONOMA DE SANTO DOMINGO (UASD) </h1>" +
+                "<p> DIPLOMADO, DESARROLLO WEB.. MVC. </p>" +
+                 "</body></html>";
+        }
+        public string DiplomadoWeB()
+        {
+            return "<html><body>" +
+                "<h1> ESTUDIANTES </h1>" +
+                "<p>RONALD PEÑA NUÑEZ<br>" +
+                "HENRY PICHARDO" +
+                "EDUARD TORRES<br>" +
+                "MARBEL BATISTA < br > " +
+                  "YORDY DE LA CRUZ < br > " +
+                    "EZEQUIAS GOMEZ < br > " +
+                      "VICENTE MONTERO< br > " +
+                        "MIGUEL REYES < br > " +
+                          "URIEL MATOS < br > " +
+                            "YERISON PEREZ < br > " +
+                            "</body></html>" ;
+        }
+    }
+}
+
+        
+
+    
---
+++ Ejercicio1_Unidad_2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Ejercicio1_Unidad_2.MvcApplication" Language="C#" %>
